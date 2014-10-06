778ea5d4c2cf83640a1357854ad789200f80f3f2
Garreth Nedved <gnedved@mail.csuchico.edu>
2014-10-06 03:51:19 +0000

    Add home and signup controllers and views

--- assassin_app/config/routes.rb
+++ assassin_app/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'users/new'
+
+  root 'home#index'
+
+  match '/signup', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
