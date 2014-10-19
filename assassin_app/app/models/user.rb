438f76e41ca046b6a73bb455abacab87e050a087
Garreth Nedved <gnedved@mail.csuchico.edu>
2014-10-19 06:36:55 +0000

    Add google authentication, still not working

--- assassin_app/app/models/user.rb
+++ assassin_app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
  
-  validates :nickname, presence: true, length: { maximum: 50 }
+  # validates :nickname, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
 
@@ -12,4 +12,20 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, length: { minimum: 6 }
+
+  # Google Auth Stuff
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
+
+
 end
